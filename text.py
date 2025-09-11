e5fa062ccd7cd3183b48734fb15124202ec35cb6
utkarsh924 <us0304619@gmail.com>
2025-09-11 16:53:06 +0530

    initial commit of python project

--- text.py
+++ text.py
@@ -1,5 +1,10 @@
-a=30
-b=50
-c=70
-print(a+b+c)
-print("hellow world")
+def reverse_number(num):
+    reversed_num = 0
+    while num > 0:
+        reversed_num = reversed_num * 10 + num % 10
+        num = num // 10
+    return reversed_num
+
+# Example usage
+number = int(input("Enter a number: "))
+print("Reversed number:", reverse_number(number))
