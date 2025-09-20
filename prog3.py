23fe988a333665a30a525ee49a59825a854a8585
utkarsh924 <us0304619@gmail.com>
2025-09-20 07:23:47 +0530

    upload this coad

---
+++ prog3.py
@@ -0,0 +1,15 @@
+# login program and indentation
+# email-> us0304619@gmail.com
+# password-> utkarsh19
+
+
+email=input("Enter the email")
+password=input("Enter the password")
+print(email,password)
+
+if email== 'us0304619@gmail.com' and password== 'utkarsh19':
+    print("welcome on your facebook account")
+
+else:
+    print("not correct")
+
