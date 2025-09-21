19c9ba8bef94e72718db9c81646c764a699dbca0
utkarsh924 <us0304619@gmail.com>
2025-09-21 17:34:14 +0530

    upload this code

---
+++ prog6.py
@@ -0,0 +1,16 @@
+# Menu driven ATM
+menu = input('''''
+Hi! how can i help you.
+1. Enter 1 for pin change
+2. Enter 2 for Widthdrawl
+3. Enter 3 for balance check
+4. Enter 4 for exit''')
+
+if menu== '1' :
+    print('pin change for more security and fakes')
+elif menu== '2' :
+    print('for widthdrawl your money easily and safe')
+elif menu== '3' :
+    print('Please check your balance amount on your ATM')
+else:
+    print('press cancle for exit and thnks for coming')
