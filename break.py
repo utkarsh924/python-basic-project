440fc36b1d0b92a1f3036eaaaf227965e173c87a
utkarsh924 <us0304619@gmail.com>
2025-10-05 17:27:59 +0530

    upload all files and update previous files on github

---
+++ break.py
@@ -0,0 +1,6 @@
+# Break function
+
+for i in range(1,10):
+    if i==5:
+        break
+    print(i)
---
+++ break2.py
@@ -0,0 +1,11 @@
+# Find out the prime numbers between 10 to 100 with using break function.
+
+lower = int(input('Enter lower range:'))
+upper = int(input('Enter upper range:'))
+
+for i in range (lower, upper+1):
+  for j in range(2,i):
+    if i%j ==0:
+      break
+  else:
+    print(i)
