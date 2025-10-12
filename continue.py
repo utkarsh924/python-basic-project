2941489b437a8c78cd979389e9204325d5524fd0
utkarsh924 <us0304619@gmail.com>
2025-10-12 07:08:05 +0530

    update all files

---
+++ continue.py
@@ -0,0 +1,6 @@
+# continue functions
+
+for i in range(1,30):
+    if i == 5:
+        continue
+    print(i)
---
+++ string_function/string1.py
@@ -0,0 +1,11 @@
+# Strings are sequence of characters
+
+# In python specially,strings are sequence of Unicode characters
+
+# . Creating Strings
+# . Accessing Strings
+# . Adding Chars to Strings
+# . Editing Strings
+# . Deleting Strings
+# . Operations on Strings
+# . String Functions
